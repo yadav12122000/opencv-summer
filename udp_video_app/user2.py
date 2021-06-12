bbd33d2bfbcdd864b25727fc48656aff80f9b207
Lalit Yadav <69167025+yadav12122000@users.noreply.github.com>
2021-06-12 21:03:34 +0530

    Update user2.py

--- udp_video_app/user2.py
+++ udp_video_app/user2.py
@@ -1,10 +1,14 @@
-import cv2, socket, pickle, os,threading
+import cv2
+import socket
+import pickle
+import threading
+
 def sender():
     s=socket.socket(socket.AF_INET , socket.SOCK_DGRAM)
     s.setsockopt(socket.SOL_SOCKET, socket.SO_SNDBUF, 65536)
-    serverip="192.168.43.229" #Server IP
+    serverip="192.168.43.229"
     serverport=3000 
-    url = "http://192.168.43.1:8080/video"         #Server Port
+    url = "http://192.168.43.1:8080/video"     
     cap = cv2.VideoCapture(url)
     while True:
         ret,photo = cap.read()       
@@ -23,8 +27,8 @@ def sender():
 
 def receiver():
     sr=socket.socket(socket.AF_INET , socket.SOCK_DGRAM)
-    ip="192.168.43.229"      #Host IP
-    port=3050               #Host Port
+    ip="192.168.43.229"      
+    port=3050             
     sr.bind((ip,port))
     
     while True:
@@ -32,12 +36,9 @@ def receiver():
         if x == 0:
             pass
         else:
-            # print(x)
             clientip = x[1][0]
             data=x[0]
-            # print(data)
             data=pickle.loads(data)
-            # print(type(data))
             data = cv2.imdecode(data, cv2.IMREAD_COLOR)
             cv2.imshow('receiver1', data) 
             if cv2.waitKey(10) == 13:
@@ -46,7 +47,7 @@ def receiver():
 
 
 
-t1 = threading.Thread(target=sender)
-t2 = threading.Thread(target=receiver)
-t1.start()
-t2.start()
+x1 = threading.Thread(target=sender)
+x2 = threading.Thread(target=receiver)
+x1.start()
+x2.start()
